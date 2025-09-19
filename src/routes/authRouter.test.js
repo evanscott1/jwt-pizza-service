54c168c297542340c4756c9525e92dd250e994ec
Evan Scott <157188169+evanscott1@users.noreply.github.com>
2025-09-19 14:07:19 -0600

    Chore: Remove unused imports

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../service');
-const { Role, DB } = require('../database/database.js');
+const { DB } = require('../database/database.js');
 
 
 if (process.env.VSCODE_INSPECTOR_OPTIONS) {
@@ -43,17 +43,4 @@ test('logout', async () => {
 
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-}
-
-function randomName() {
-  return Math.random().toString(36).substring(2, 12);
-}
-
-async function createAdminUser() {
-  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-  user.name = randomName();
-  user.email = user.name + '@admin.com';
-
-  user = await DB.addUser(user);
-  return { ...user, password: 'toomanysecrets' };
 }
